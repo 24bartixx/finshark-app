34290c991db652d06f1af7967ac823c8405a2589
Bartosh <jestgitbartix@gmail.com>
2025-07-15 00:01:58 +0200

    feat: company profile hook

--- frontend/src/api/companies.tsx
+++ frontend/src/api/companies.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "../types/company";
+import { CompanyProfile, CompanySearch } from "../types/company";
 
 interface SerachResponse {
   data: CompanySearch[];
@@ -19,5 +19,16 @@ export const searchCompanies = async (query: string) => {
       console.log("Unexpected error: ", error);
       return "An unexpected error has occured.";
     }
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("Error message from API: ", error.message);
   }
 };
--- frontend/src/pages/company-page/CompanyPage.tsx
+++ frontend/src/pages/company-page/CompanyPage.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CompanyProfile } from "../../types/company";
+import { getCompanyProfile } from "../../api/companies";
 
 type Props = {};
 
 const CompanyPage = (props: Props) => {
-  return <div>CompanyPage</div>;
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const result = await getCompanyProfile(
+        ticker === undefined ? "" : ticker
+      );
+      setCompany(result?.data[0]);
+    };
+    getProfile();
+  }, [ticker]);
+
+  return (
+    <>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
