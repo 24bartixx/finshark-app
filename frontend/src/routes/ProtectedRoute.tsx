dde75f6029beafac4081fd1234923a689987cfd0
Bartosh <jestgitbartix@gmail.com>
2025-08-02 14:04:46 +0200

    feat(front): protect routes

---
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useAuth } from "../context/useAuth";
+import { Navigate, useLocation } from "react-router-dom";
+
+type Props = { children: React.ReactNode };
+
+const ProtectedRoute = ({ children }: Props) => {
+  const { isLoggedIn } = useAuth();
+  const location = useLocation();
+
+  return isLoggedIn() ? (
+    <>{children}</>
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+};
+
+export default ProtectedRoute;
